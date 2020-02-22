6df7afbf7003fa554b1c6ea873042e8beb6d644a
Todd Spainhour <toddspainhour@gmail.com>
2020-02-22 10:22:55 -0600

    added form to array and reset

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("Your JS is connected!")
+
+const dinos = [];
+
+const newDino = (e) => {
+    e.preventDefault();
+    const brandNewDino = {
+        id: `dinos${dinos.length +1}`,
+        name: document.getElementById('dino-name').value,
+        type: document.getElementById('dino-type').value,
+        age: document.getElementById('dino-age').value,
+        owner: document.getElementById('dino-owner').value,
+        adventures: [],
+        health: 100,
+        image: document.getElementById('dino-image').value,
+    }
+    dinos.push(brandNewDino);
+    document.getElementById('new-dino-form').reset();
+    console.log('hi from new dino, brandNewDino');
+};
+
+const init = () => {
+    document.getElementById('submit-new-dino').addEventListener('click', newDino);
+    
+}
+
+init();
